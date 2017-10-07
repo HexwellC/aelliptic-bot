de7667389d38d097996f9512a5de3a3855361e6a
HexwellC <hexwellc@rape.lol>
2017-10-07 14:36:56 +0300

    Handling exceptions instead of checking twice if string is UTF-8 correct

--- src/commands/base64.cpp
+++ src/commands/base64.cpp
@@ -62,83 +62,6 @@ std::string base64_encode(unsigned char const* bytes_to_encode,
   return ret;
 }
 
-bool is_utf8(const char * string) {
-    if(!string)
-        return 0;
-
-    const unsigned char * bytes = (const unsigned char *)string;
-    while(*bytes)
-    {
-        if( (// ASCII
-             // use bytes[0] <= 0x7F to allow ASCII control characters
-                bytes[0] == 0x09 ||
-                bytes[0] == 0x0A ||
-                bytes[0] == 0x0D ||
-                (0x20 <= bytes[0] && bytes[0] <= 0x7E)
-            )
-        ) {
-            bytes += 1;
-            continue;
-        }
-
-        if( (// non-overlong 2-byte
-                (0xC2 <= bytes[0] && bytes[0] <= 0xDF) &&
-                (0x80 <= bytes[1] && bytes[1] <= 0xBF)
-            )
-        ) {
-            bytes += 2;
-            continue;
-        }
-
-        if( (// excluding overlongs
-                bytes[0] == 0xE0 &&
-                (0xA0 <= bytes[1] && bytes[1] <= 0xBF) &&
-                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
-            ) ||
-            (// straight 3-byte
-                ((0xE1 <= bytes[0] && bytes[0] <= 0xEC) ||
-                    bytes[0] == 0xEE ||
-                    bytes[0] == 0xEF) &&
-                (0x80 <= bytes[1] && bytes[1] <= 0xBF) &&
-                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
-            ) ||
-            (// excluding surrogates
-                bytes[0] == 0xED &&
-                (0x80 <= bytes[1] && bytes[1] <= 0x9F) &&
-                (0x80 <= bytes[2] && bytes[2] <= 0xBF)
-            )
-        ) {
-            bytes += 3;
-            continue;
-        }
-
-        if( (// planes 1-3
-                bytes[0] == 0xF0 &&
-                (0x90 <= bytes[1] && bytes[1] <= 0xBF) &&
-                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
-                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
-            ) ||
-            (// planes 4-15
-                (0xF1 <= bytes[0] && bytes[0] <= 0xF3) &&
-                (0x80 <= bytes[1] && bytes[1] <= 0xBF) &&
-                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
-                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
-            ) ||
-            (// plane 16
-                bytes[0] == 0xF4 &&
-                (0x80 <= bytes[1] && bytes[1] <= 0x8F) &&
-                (0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
-                (0x80 <= bytes[3] && bytes[3] <= 0xBF)
-            )
-        ) {
-            bytes += 4;
-            continue;
-        }
-        return false;
-    }
-    return true;
-}
-
 std::string base64_decode(std::string const& encoded_string) {
     int in_len = encoded_string.size();
     int i = 0;
@@ -197,11 +120,6 @@ namespace aelliptic { namespace commands {
         std::string response;
         if(tokens[1] == "decode") {
             response = base64_decode(tokens[2]);
-            if (!is_utf8(response.c_str())) {
-                bot->getApi().sendMessage(message->chat->id, 
-                                          "ERROR: Invalid base64 string");
-                return;
-            }
         } else if (tokens[1] == "encode") {
             response = base64_encode(reinterpret_cast
                                     <const unsigned char*>(tokens[2].c_str()), 
@@ -210,6 +128,11 @@ namespace aelliptic { namespace commands {
             useage(message->chat->id);
             return;
         }
-        bot->getApi().sendMessage(message->chat->id, response);
+        try {
+            bot->getApi().sendMessage(message->chat->id, response);
+        } catch (...) { 
+            bot->getApi().sendMessage(message->chat->id, 
+                                      "ERROR: Invalid input");
+        }
     }
 }}
